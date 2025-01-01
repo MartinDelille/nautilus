121eec4f46d1c4517d43cfe934cde244373ece79
Martin Delille <martin@delille.org>
2025-01-01 17:44:13 +0100

    Add wave to the water

--- cube.gd
+++ cube.gd
@@ -4,23 +4,16 @@ extends RigidBody3D
 @export var water_drag := 0.05
 @export var water_angular_drag := 0.05
 @onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var water = get_node('/root/Map/WaterPlane')
 
-const water_height:= 0.0
+var submerged = false
 
-var submerged=false
-
-func _ready() -> void:
-	pass # Replace with function body.
-
-func _process(delta: float) -> void:
-	pass
-
-func _physics_process(delta: float) -> void:
-	submerged=false
-	var depth = water_height - global_position.y
+func _physics_process(_delta: float) -> void:
+	submerged = false
+	var depth = water.get_height(global_position) - global_position.y
 	if depth > 0:
-		apply_central_force(Vector3.UP*floating_force*gravity*depth)
-		submerged=true
+		apply_central_force(Vector3.UP * floating_force * gravity * depth)
+		submerged = true
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	if submerged:
