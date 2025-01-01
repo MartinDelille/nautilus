ea75a9f6183000c94d3e0135c40f4d67c8c5f494
Martin Delille <martin@delille.org>
2025-01-01 18:16:51 +0100

    Apply floating force to probes

--- cube.gd
+++ cube.gd
@@ -1,19 +1,22 @@
 extends RigidBody3D
 
-@export var floating_force:=1.0
+@export var floating_force := 1.4
 @export var water_drag := 0.05
 @export var water_angular_drag := 0.05
 @onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var water = get_node('/root/Map/WaterPlane')
 
+@onready var probes = $ProbeContainer.get_children()
+
 var submerged = false
 
 func _physics_process(_delta: float) -> void:
 	submerged = false
-	var depth = water.get_height(global_position) - global_position.y
-	if depth > 0:
-		apply_central_force(Vector3.UP * floating_force * gravity * depth)
-		submerged = true
+	for p in probes:
+		var depth = water.get_height(p.global_position) - p.global_position.y
+		if depth > 0:
+			apply_force(Vector3.UP * floating_force * gravity * depth, p.global_position - global_position)
+			submerged = true
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	if submerged:
