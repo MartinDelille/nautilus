d26ebd9e01593ed91a0c44b4dbb69c831c74436b
Martin Delille <martin@delille.org>
2025-03-23 18:18:05 +0100

    Wind has both degree and radiant value

--- wind.gd
+++ wind.gd
@@ -1,12 +1,16 @@
+@tool
 extends Node3D
 
-@export var wind_direction := 0.
+@export var wind_angle_degree := 0.
 @export var wind_intensity := 3.
 
+@export var wind_angle_radiant: float:
+	get:
+		return wind_angle_degree * PI / 180
 @export var wind_vector: Vector3:
 	get:
-		return Vector3.RIGHT * Quaternion(Vector3.UP, wind_direction * PI / 180) * wind_intensity
+		return Vector3.RIGHT * Quaternion(Vector3.UP, wind_angle_radiant) * wind_intensity
 
 
 func _process(_delta: float) -> void:
-	$Arrow.rotation = Vector3(0, -wind_direction * PI / 180, 0)
+	$Arrow.rotation.y = -wind_angle_radiant
